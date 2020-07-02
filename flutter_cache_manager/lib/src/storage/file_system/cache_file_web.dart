b9ff78f6e2c95ddce8ee673f04059b87609e01b9
Gábor DEÁK JAHN <deakjahn@gmail.com>
2020-07-02 19:27:09 +0200

    Add files via upload

--- flutter_cache_manager/lib/src/storage/file_system/cache_file_web.dart
+++ flutter_cache_manager/lib/src/storage/file_system/cache_file_web.dart
@@ -5,10 +5,12 @@ import 'dart:js' as js;
 import 'dart:typed_data';
 
 import 'package:file/file.dart';
+import 'package:synchronized/synchronized.dart';
 
 import 'cache_file.dart' as def;
 
 class CacheFile implements def.CacheFile, IOSink {
+  static final Lock lock = Lock();
   js.JsObject _cache;
   String _name;
 
@@ -49,50 +51,52 @@ class CacheFile implements def.CacheFile, IOSink {
 
   @override
   Future writeAsBytes(Uint8List bytes) async {
-print('writeAsBytes start');
-    // JS: var blob = new Blob([bytes], {type : mime});
-    final array = js.JsObject.jsify([bytes]);
-    final options1 = js.JsObject.jsify({
-      'mime': 'image/*',
+    await lock.synchronized(() async {
+      // JS: var blob = new Blob([bytes], {type : mime});
+      final array = js.JsObject.jsify([bytes]);
+      final options1 = js.JsObject.jsify({
+        'mime': 'image/*',
+      });
+      final blob = js.JsObject(js.context['Blob'], [array, options1]);
+  
+      // JS: var options = {headers: {'Content-Type': mime, 'Content-Length': bytes.length}};
+      final options2 = js.JsObject.jsify({
+        'headers': {
+          'Content-Type': 'image/*',
+          'Content-Length': bytes.length,
+        },
+      });
+  
+      // JS: await cache.put(path, new Response(blob, options));
+      final response = js.JsObject(js.context['Response'], [blob, options2]);
+      final promise = _cache.callMethod('put', [_name, response]);
+      await _toFuture(promise);
     });
-    final blob = js.JsObject(js.context['Blob'], [array, options1]);
-
-    // JS: var options = {headers: {'Content-Type': mime, 'Content-Length': bytes.length}};
-    final options2 = js.JsObject.jsify({
-      'headers': {
-        'Content-Type': 'image/*',
-        'Content-Length': bytes.length,
-      },
-    });
-
-    // JS: await cache.put(path, new Response(blob, options));
-    final response = js.JsObject(js.context['Response'], [blob, options2]);
-    final promise = _cache.callMethod('put', [_name, response]);
-    await _toFuture(promise);
-print('writeAsBytes end');
   }
 
   @override
   Future<Uint8List> readAsBytes() async {
-print('readAsBytes start');
-    try {
-      // JS: var data = await cache.match(name, {ignoreSearch: true, ignoreMethod: true, ignoreVary: true});
-      final promise1 = _cache.callMethod('match', [_name, nameOptions]);
-      final response = await _toFuture<js.JsObject>(promise1);
-print(response);
-  
-      // JS: return data.arrayBuffer();
-      final promise2 = response.callMethod('arrayBuffer');
-      final data = await _toFuture<ByteBuffer>(promise2);
-print('data ${data.lengthInBytes}');
-      return data.asUint8List();
-    }
-    catch (e) {
-      print(e);
-    }
-    finally {
-print('readAsBytes end');
-    }
+    return await lock.synchronized(() async {
+      try {
+        print('readAsBytes start');
+        // JS: var data = await cache.match(name, {ignoreSearch: true, ignoreMethod: true, ignoreVary: true});
+        final promise1 = _cache.callMethod('match', [_name, nameOptions]);
+        final response = await _toFuture<js.JsObject>(promise1);
+        print(response);
+    
+        // JS: return data.arrayBuffer();
+        final promise2 = response.callMethod('arrayBuffer');
+        final data = await _toFuture<ByteBuffer>(promise2);
+        print('data ${data.lengthInBytes}');
+        return data.asUint8List();
+      }
+      catch (e) {
+        print(e);
+      }
+      finally {
+        print('readAsBytes end');
+      }
+    });
   }
 
   Future<T> _toFuture<T>(js.JsObject promise) {
